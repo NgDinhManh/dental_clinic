c261e32a23c0f34a5a31fb6101f331f956cbc8a6
NDMANH <manhngao258@gmail.com>
2025-10-21 09:37:07 +0700

    feat: dmanh tạo migration create_messages_table

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -43,7 +43,7 @@
 
             <div class="col-lg-6 map-wrapper mb-5 p-0" data-aos="fade-up" data-aos-delay="200">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d917.6738625675167!2d105.6813217324758!3d18.711212222748866!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3139cde2c01aeea3%3A0x27a247390eebf2e!2zMTIgVHLGsMahbmcgVsSDbiBMxKluaCwgSMOgIEh1eSBU4bqtcCwgVmluaCwgTmdo4buHIEFuLCBWaeG7h3QgTmFt!5e1!3m2!1svi!2s!4v1748786968810!5m2!1svi!2s"
-                    width="100%" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>      
+                    width="100%" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div><!-- End Google Maps -->
 
             <style>
@@ -82,7 +82,7 @@
                 </div>
 
                 <div class="col-md-12">
-                  <textarea class="form-control" name="message" rows="6" placeholder="Nội dung" required=""></textarea>
+                  <textarea class="form-control" name="content" rows="6" placeholder="Nội dung" required=""></textarea>
                 </div>
 
                 <div class="col-md-12 text-center">
